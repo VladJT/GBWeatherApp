83a488a99c5eda68d0d638c6d94847f2357628a4
Богданков Владислав <onlyrus@hotmail.com>
2022-07-18 21:38:43 +0300

    добавлен список контактов

--- app/src/main/java/jt/projects/gbweatherapp/model/room/WeatherDAO.kt
+++ app/src/main/java/jt/projects/gbweatherapp/model/room/WeatherDAO.kt
@@ -5,34 +5,15 @@ import androidx.room.*
 
 @Dao
 interface WeatherDAO {
+    // ** WeatherEntity **
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(weatherEntity: WeatherEntity)
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertHistory(weatherHistoryEntity: WeatherHistoryEntity)
-
-//    @Query(
-//        "Insert into weather_entity_table(name,lat,lon,temperature,feelsLike,condition,icon) " +
-//                "values (:name,:lat,:lon,:temperature,:feelsLike,:condition,:icon)"
-//    )
-//    fun insertNative(
-//        name: String,
-//        lat: Double,
-//        lon: Double,
-//        temperature: Int,
-//        feelsLike: Int,
-//        condition: String, icon: String
-//    )
-
 
     @Query("SELECT * FROM weather_entity_table")
     fun getWeatherAll(): List<WeatherEntity>
 
     @Query("SELECT * FROM weather_entity_table where lat=:lat and lon=:lon")
     fun getWeatherByLocation(lat: Double, lon: Double): List<WeatherEntity>
-
-    @Query("SELECT * FROM weather_history_table where lat=:lat and lon=:lon order by id asc")
-    fun getHistoryByLocation(lat: Double, lon: Double): List<WeatherHistoryEntity>
 
     @Update
     fun update(entity: WeatherEntity)
@@ -46,9 +27,22 @@ interface WeatherDAO {
     @Query("DELETE FROM weather_entity_table WHERE id = :id")
     fun deleteById(id: Long)
 
+
+    // ** WeatherHistoryEntity **
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertHistory(weatherHistoryEntity: WeatherHistoryEntity)
+
+    @Update
+    fun updateHistory(entity: WeatherHistoryEntity)
+
     @Query("SELECT id, lat, lon, temperature FROM weather_entity_table")
     fun getHistoryCursor(): Cursor
 
     @Query("SELECT id, lat, lon, temperature FROM weather_entity_table WHERE id = :id")
     fun getHistoryCursor(id: Long): Cursor
+
+    @Query("SELECT * FROM weather_history_table where lat=:lat and lon=:lon order by id asc")
+    fun getHistoryByLocation(lat: Double, lon: Double): List<WeatherHistoryEntity>
+
 }
--- app/src/main/java/jt/projects/gbweatherapp/ui/contacts/ContactsFragment.kt
+++ app/src/main/java/jt/projects/gbweatherapp/ui/contacts/ContactsFragment.kt
@@ -1,8 +1,8 @@
 package jt.projects.gbweatherapp.ui.contacts
 
 import android.Manifest
-import android.content.ContentResolver
 import android.content.pm.PackageManager
+import android.content.res.Resources
 import android.database.Cursor
 import android.os.Bundle
 import android.provider.ContactsContract
@@ -14,7 +14,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import jt.projects.gbweatherapp.R
 import jt.projects.gbweatherapp.databinding.FragmentContactsBinding
+import jt.projects.gbweatherapp.utils.showSnackBarShort
 
 const val REQUEST_CODE = 42
 
@@ -49,46 +51,57 @@ class ContactsFragment : Fragment() {
     }
 
     private fun checkPermission() {
-        context?.let {
-            when {
-                ContextCompat.checkSelfPermission(it, Manifest.permission.READ_CONTACTS) ==
-                        PackageManager.PERMISSION_GRANTED -> {
-                    //1) Проверяем случай, если разрешение на доступ к контактам уже дано. Если это так, то
-                    //вызываем Content Provider для получения контактов. Как мы уже писали выше, потребуется
-                    //каждый раз проверять разрешение, потому что в любой момент пользователь может его
-                    //отменить.
-                    getContacts()
-                }
-                //2) Опционально: Отображаем пояснение перед запросом разрешения. Вызываем этот метод, если считаем
-                //важным пояснить пользователю приложения, зачем нам требуется доступ к данным на
-                //смартфоне. Обычно в роли пояснения выступает диалоговое окно, всплывающее перед тем,
-                //как запрашивать доступ
-                shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS) -> {
-                    AlertDialog.Builder(it)
-                        .setTitle("Доступ к контактам")
-                        .setMessage("Объяснение")
-                        .setPositiveButton("Предоставить доступ") { _, _ ->
-                            requestPermission()
-                        }
-                        .setNegativeButton("Не надо") { dialog, _ ->
-                            dialog.dismiss()
-                        }
-                        .create()
-                        .show()
-                }
-                else -> {
-                    //3) Если разрешения нет и объяснение отображать не надо, запрашиваем разрешение.
-                    requestPermission()
-                }
 
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.READ_CONTACTS
+            ) ==
+            PackageManager.PERMISSION_GRANTED
+        ) {
+            //1) Проверяем случай, если разрешение на доступ к контактам уже дано. Если это так, то
+            //вызываем Content Provider для получения контактов. Как мы уже писали выше, потребуется
+            //каждый раз проверять разрешение, потому что в любой момент пользователь может его
+            //отменить.
+            getContacts()
+        } else
+
+        //2) Опционально: Отображаем пояснение перед запросом разрешения. Вызываем этот метод, если считаем
+        //важным пояснить пользователю приложения, зачем нам требуется доступ к данным на
+        //смартфоне. Обычно в роли пояснения выступает диалоговое окно, всплывающее перед тем,
+        //как запрашивать доступ
+            if (shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS)) {
+                AlertDialog.Builder(requireContext())
+                    .setTitle("Доступ к контактам")
+                    .setMessage("Прошу дать доступ...")
+                    .setPositiveButton("Предоставить доступ") { _, _ ->
+                        requestPermission()
+                    }
+                    .setNegativeButton("Не надо") { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    .create()
+                    .show()
+            } else {
+                //3) Если разрешения нет и объяснение отображать не надо, запрашиваем разрешение.
+                requestPermission()
             }
-        }
     }
 
     private fun requestPermission() {
         // метод запроса разрешений принимает список разрешений — можно запрашивать сразу
         //несколько — и request code, по которому будет определять разрешения.
         requestPermissions(arrayOf(Manifest.permission.READ_CONTACTS), REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE) {
+            getContacts()
+        }
     }
 
     private fun getContacts() {
@@ -108,16 +121,23 @@ class ContactsFragment : Fragment() {
                 ContactsContract.Contacts.DISPLAY_NAME + " ASC"
             )
             cursorWithContacts?.let { cursor ->
-                for (i in 0..cursor.count) {
+                for (i in 0 until cursor.count) {
                     // Переходим на позицию в Cursor
                     if (cursor.moveToPosition(i)) {
                         // Берём из Cursor столбец с именем
                         val name =
                             cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
 
-                        binding.containerForContacts.addView(AppCompatTextView(it).apply {
+                        val contactView = binding.containerForContacts.addView(AppCompatTextView(it).apply {
                             text = name
-                            textSize = 12f
+                            textSize = 16f
+                            setOnClickListener { binding.root.showSnackBarShort(name) }
+                        })
+
+                        // divider
+                        binding.containerForContacts.addView(AppCompatTextView(it).apply {
+                           height = 2
+                           setBackgroundColor(R.color.black)
                         })
                     }
                 }
